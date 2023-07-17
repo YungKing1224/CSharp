c4e19e471340b7194b7d43fece5e5e0a8e7a260e
Jason <111362966+YungKing1224@users.noreply.github.com>
2023-07-17 16:04:11 +0800

    Add files via upload
    The provided code is a basic console application written in C#. It implements a simple calculator that performs arithmetic operations based on the user's input.
    
    Here's how the code works:
    
    1. The necessary namespace `System` is included to use the `Console` class.
    2. The code is defined within the `HelloWorld` namespace.
    3. The main logic is implemented within the `Hello` class.
    4. The entry point of the program is the `Main` method, which takes an array of strings as input parameters.
    5. Inside the `Main` method, three integer variables `num1`, `num2`, and `ans` are declared to store the input numbers and the result of the operation.
    6. The program displays a message asking the user to select an operation: "Basic Calculator: 1. ADD 2. Mul 3. Div 4. Sub".
    7. The user's input is read as a string using `Console.ReadLine()`, and then it is converted to an integer using `Convert.ToInt32()`.
    8. Based on the user's input, the program executes the corresponding block of code:
       - If the input is 1, the program asks the user for two numbers, adds them together, and displays the result.
       - If the input is 2, the program asks the user for two numbers, multiplies them, and displays the result.
       - If the input is 3, the program asks the user for two numbers, performs division, and displays the result.
       - If the input is 4, the program asks the user for two numbers, subtracts them, and displays the result.
       - If the input is none of the above, an "Invalid Input!!" message is displayed.
    9. The program execution ends after the selected operation is performed and the result is displayed.
    
    The code provides a basic calculator functionality, allowing the user to choose between different arithmetic operations and obtain the corresponding results based on their input.

---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Hello
+    {
+        static void Main(string[] args)
+        {
+           int num1, num2, ans;
+            Console.WriteLine("Basic Calculator: 1. ADD 2. Mul 3. Div 4. Sub");
+            int input = Convert.ToInt32(Console.ReadLine());
+            if (input == 1)
+            {
+                //Input numbers
+                Console.WriteLine("1st Input: ");
+                num1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("2nd Input: ");
+                num2 = Convert.ToInt32(Console.ReadLine());
+
+                //Do operation
+                ans = num1 + num2;
+                Console.WriteLine($"{num1} + {num2} = {ans}"); 
+            }
+            else if (input == 2)
+             {
+                //Input numbers
+                Console.WriteLine("1st Input: ");
+                num1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("2nd Input: ");
+                num2 = Convert.ToInt32(Console.ReadLine());
+
+                //DO operation
+                ans = num1 * num2;
+                Console.WriteLine($"{num1} * {num2} = {ans}");
+             }
+            else if (input == 3)
+            {
+                //Input numbers
+                Console.WriteLine("1st Input: ");
+                num1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("2nd Input: ");
+                num2 = Convert.ToInt32(Console.ReadLine());
+
+                //Do operation
+                ans = num1 / num2;
+                Console.WriteLine($"{num1} / {num2} = {ans}");
+            }
+            else if (input == 4)
+            {
+                //Input numbers
+                Console.WriteLine("1st Input: ");
+                num1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("2nd Input: ");
+                num2 = Convert.ToInt32(Console.ReadLine());
+
+                //Do operation
+                ans = num1 - num2;
+                Console.WriteLine($"{num1} - {num2} = {ans}");
+            }
+            else
+            {
+                Console.WriteLine("Invalid Input!!");
+            }
+        }
+    }
+}
